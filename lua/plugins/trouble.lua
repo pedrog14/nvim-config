142ea6e9265a483365f0524f69cb13a3e048f5cf
Pedro Gabriel de Morais Ribeiro <leo140201@hotmail.com>
2024-10-01 16:51:50 -0300

    Updated README.md

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,61 +2,58 @@ return {
     "folke/trouble.nvim",
     dependencies = "nvim-tree/nvim-web-devicons",
     cmd = "Trouble",
-    keys = function()
-        local toggle = require("trouble").toggle
-        return {
-            {
-                "<leader>xx",
-                function()
-                    toggle({ mode = "diagnostics" })
-                end,
-                desc = "Diagnostics (Trouble)",
-            },
-            {
-                "<leader>xX",
-                function()
-                    toggle({
-                        mode = "diagnostics",
-                        filter = { buf = 0 },
-                    })
-                end,
-                desc = "Buffer Diagnostics (Trouble)",
-            },
-            {
-                "<leader>cs",
-                function()
-                    toggle({ mode = "symbols", focus = false })
-                end,
-                desc = "Symbols (Trouble)",
-            },
-            {
-                "<leader>cl",
-                function()
-                    toggle({
-                        mode = "lsp",
-                        focus = false,
-                        win = {
-                            position = "right",
-                        },
-                    })
-                end,
-                desc = "LSP Definitions / references / ... (Trouble)",
-            },
-            {
-                "<leader>xL",
-                function()
-                    toggle({ mode = "loclist" })
-                end,
-                desc = "Location List (Trouble)",
-            },
-            {
-                "<leader>xQ",
-                function()
-                    toggle({ mode = "qflist" })
-                end,
-                desc = "Quickfix List (Trouble)",
-            },
-        }
-    end,
+    keys = {
+        {
+            "<leader>xx",
+            function()
+                require("trouble").toggle({ mode = "diagnostics" })
+            end,
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xX",
+            function()
+                require("trouble").toggle({
+                    mode = "diagnostics",
+                    filter = { buf = 0 },
+                })
+            end,
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>cs",
+            function()
+                require("trouble").toggle({ mode = "symbols", focus = false })
+            end,
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>cl",
+            function()
+                require("trouble").toggle({
+                    mode = "lsp",
+                    focus = false,
+                    win = {
+                        position = "right",
+                    },
+                })
+            end,
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+            "<leader>xL",
+            function()
+                require("trouble").toggle({ mode = "loclist" })
+            end,
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>xQ",
+            function()
+                require("trouble").toggle({ mode = "qflist" })
+            end,
+            desc = "Quickfix List (Trouble)",
+        },
+    },
     opts = {},
 }
