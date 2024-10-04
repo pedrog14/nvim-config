953a4ceeb6042cb060dbbec29738302f98e39ac6
Pedro Gabriel de Morais Ribeiro <leo140201@hotmail.com>
2024-10-04 16:12:03 -0300

    Added conform.nvim + mason-conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            format_on_save = {
+                -- These options will be passed to conform.format()
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
+        },
+    },
+
+    {
+        "pedrog14/mason-conform.nvim",
+        opts = function(_, opts)
+            opts.handlers =
+                require("mason-conform").default_handlers(opts.ensure_installed)
+        end,
+    },
+}
