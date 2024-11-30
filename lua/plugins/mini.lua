8836b3ef52e88ddda88b8a6f0397148ab8ee8719
Pedro Gabriel de Morais Ribeiro <leo140201@hotmail.com>
2024-11-30 18:31:12 -0300

    change: removed 'FIXME' highlights

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -19,24 +19,6 @@ return {
             local utils = require("utils").plugins.mini.hipatterns
             return {
                 highlighters = {
-                    -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-                    fixme = {
-                        pattern = "%f[%w]()FIXME()%f[%W]",
-                        group = "MiniHipatternsFixme",
-                    },
-                    hack = {
-                        pattern = "%f[%w]()HACK()%f[%W]",
-                        group = "MiniHipatternsHack",
-                    },
-                    todo = {
-                        pattern = "%f[%w]()TODO()%f[%W]",
-                        group = "MiniHipatternsTodo",
-                    },
-                    note = {
-                        pattern = "%f[%w]()NOTE()%f[%W]",
-                        group = "MiniHipatternsNote",
-                    },
-
                     hex_color = hipatterns.gen_highlighter.hex_color({
                         priority = 2000,
                     }),
