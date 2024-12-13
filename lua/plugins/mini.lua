f5b51b751573ec55599adc277967cb44252e18c5
Pedro Gabriel <leo140201@hotmail.com>
2024-12-12 22:51:18 -0300

    new: added nvim-lint plugin

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -11,18 +11,9 @@ return {
                         a = { "@block.outer", "@conditional.outer", "@loop.outer" },
                         i = { "@block.inner", "@conditional.inner", "@loop.inner" },
                     }),
-                    f = ai.gen_spec.treesitter({ -- function
-                        a = "@function.outer",
-                        i = "@function.inner",
-                    }),
-                    c = ai.gen_spec.treesitter({ -- class
-                        a = "@class.outer",
-                        i = "@class.inner",
-                    }),
-                    t = {
-                        "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", -- tags
-                        "^<.->().*()</[^/]->$",
-                    },
+                    f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+                    c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+                    t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
                     d = { "%f[%d]%d+" }, -- digits
                     e = { -- Word with case
                         {
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -23,6 +23,7 @@ return {
         return {
             bigfile = { enabled = true },
             quickfile = { enabled = true },
+            scope = { enabled = true },
             statuscolumn = { enabled = true },
             words = { enabled = true },
             indent = {
