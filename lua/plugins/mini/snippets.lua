6de654d2cad836aef96bffaecb4fe30076e2833f
Pedro Gabriel de Morais Ribeiro <leo140201@hotmail.com>
2025-01-19 14:27:31 -0300

    change(mini.snippets): Session should stop after jumping to final stop/when switching to Normal Mode

--- lua/plugins/mini/snippets.lua
+++ lua/plugins/mini/snippets.lua
@@ -3,6 +3,31 @@ return {
     dependencies = "rafamadriz/friendly-snippets",
     lazy = true,
     opts = function()
+        -- Stop all sessions on Normal mode exit
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "MiniSnippetsSessionStart",
+            callback = function()
+                vim.api.nvim_create_autocmd("ModeChanged", {
+                    pattern = "*:n",
+                    once = true,
+                    callback = function()
+                        while MiniSnippets.session.get() do
+                            MiniSnippets.session.stop()
+                        end
+                    end,
+                })
+            end,
+        })
+        -- Stop session immediately after jumping to final tabstop
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "MiniSnippetsSessionJump",
+            callback = function(args)
+                if args.data.tabstop_to == "0" then
+                    MiniSnippets.session.stop()
+                end
+            end,
+        })
+
         local gen_loader = require("mini.snippets").gen_loader
         return {
             expand = {
