2331edfe7a3845b347d9f1c1e52167a8e894c9b4
Pedro Gabriel <52004811+pedrog14@users.noreply.github.com>
2025-12-10 22:36:19 +0000

    change: added `utils.plugins.mini.snippets`

---
+++ lua/utils/plugins/mini/snippets.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+M.setup = function(opts)
+    -- Stop all sessions on Normal mode exit
+    vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniSnippetsSessionStart",
+        callback = function()
+            vim.api.nvim_create_autocmd("ModeChanged", {
+                pattern = "*:n",
+                once = true,
+                callback = function()
+                    while MiniSnippets.session.get() do
+                        MiniSnippets.session.stop()
+                    end
+                end,
+            })
+        end,
+    })
+
+    -- Stop session immediately after jumping to final tabstop
+    vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniSnippetsSessionJump",
+        callback = function(args)
+            if args.data.tabstop_to == "0" then
+                MiniSnippets.session.stop()
+            end
+        end,
+    })
+
+    require("mini.snippets").setup(opts)
+end
+
+return M
