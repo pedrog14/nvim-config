7b64ba788aafa140e5586a6306ee80e97d6d686c
pedrog14 <52004811+pedrog14@users.noreply.github.com>
2026-01-07 21:43:41 -0300

    change: updated settings

--- lua/plugins/colorschemes/gruvbox.lua
+++ lua/plugins/colorschemes/gruvbox.lua
@@ -1,12 +1,10 @@
 return {
-  -- "pedrog14/gruvbox.nvim",
-  dir = "~/Git/gruvbox.nvim/",
+  "pedrog14/gruvbox.nvim",
   lazy = false,
   priority = 1000,
   ---@module "gruvbox"
   ---@type GruvboxConfig
   opts = {
-    cache = false,
     group_override = function(hl, colors)
       hl.CursorLineNr = { fg = colors.yellow, bg = colors.none, bold = true }
       hl.SignColumn = { fg = colors.bg4, bg = colors.bg0 }
